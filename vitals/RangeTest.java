63b6af5c006f1a3c2462724790365c841395f7cc
prabhusns <prabhusns@gmail.com>
2022-07-06 16:26:07 +0530

    updated cod:

--- vitals/RangeTest.java
+++ vitals/RangeTest.java
@@ -8,19 +8,19 @@ public class RangeTest {
 				Constants.tempLowLimit, Constants.tempUpLimit, Constants.tempStr, true)));
 
 		assert (RangeValidator.rangeValidator.validateRange(RangeValidator.constructRangePOJO(50,
-				Constants.tempLowLimit, Constants.tempUpLimit, Constants.tempStr, true)));
+				Constants.tempLowLimit, Constants.tempUpLimit, Constants.tempStr, true)) == false);
 
 		assert (RangeValidator.rangeValidator.validateRange(RangeValidator.constructRangePOJO(50, Constants.socLowLimit,
 				Constants.socUpLimit, Constants.socStr, true)));
 
 		assert (RangeValidator.rangeValidator.validateRange(RangeValidator.constructRangePOJO(89, Constants.socLowLimit,
-				Constants.socUpLimit, Constants.socStr, true)));
+				Constants.socUpLimit, Constants.socStr, true)) == false);
 
 		assert (RangeValidator.rangeValidator.validateRange(
 				RangeValidator.constructRangePOJO(0.7f, 0f, Constants.crUpLimit, Constants.crStr, false)));
 
 		assert (RangeValidator.rangeValidator.validateRange(
-				RangeValidator.constructRangePOJO(0.9f, 0f, Constants.crUpLimit, Constants.crStr, false)));
+				RangeValidator.constructRangePOJO(0.9f, 0f, Constants.crUpLimit, Constants.crStr, false)) == false);
 
 	}
 
